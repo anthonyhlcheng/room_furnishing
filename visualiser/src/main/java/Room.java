fcb53c2a0e90f4a7f74946fa9c5572fe407f74ed
Anthony Cheng <anthonyhlcheng@gmail.com>
2017-12-11 17:17:15 +0000

    Added Test File for Room and reformatted to Test Coordinate Standards

--- visualiser/src/main/java/Room.java
+++ visualiser/src/main/java/Room.java
@@ -4,7 +4,9 @@ import javafx.util.Pair;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Line2D;
+import java.awt.geom.Path2D;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -14,15 +16,19 @@ class Room extends JPanel {
     private ArrayList<Pair<Double,Double>> coordinates = new ArrayList<>();
     private Double minX = -50.0;
     private Double minY = -50.0;
-    private int scaler = 50;
+    private static final Double SCALE = 10.0;
+    private static final Double STARTX = 50.0;
+    private static final Double STARTY = 50.0;
+
     Room(String room){
         //The string room is formatted as follows: (0,0), (0,1), (0,2) and so on...
-        String[] coordinateArray = room.trim().split(", ");
+        room = room.replace(" ","").replace("),(", ")/(");
+        String[] coordinateArray = room.split("/");
         for(String coordinate:coordinateArray){
             System.out.println(coordinate);
             int commaLocation = coordinate.indexOf(",");
             Double x = Double.parseDouble(coordinate.substring(1,commaLocation));
-            Double y = Double.parseDouble(coordinate.substring(commaLocation + 1, coordinate.length()-1));
+            Double y = Double.parseDouble(coordinate.substring(commaLocation+1, coordinate.length()-1));
             if(x < minX){
                 minX = x;
             }
@@ -31,27 +37,34 @@ class Room extends JPanel {
             }
             coordinates.add(new Pair<>(x,y));
         }
-        coordinates.add(coordinates.get(0));
     }
     private void drawRoom(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        Double lastX = null;
-        Double lastY = null;
+        AffineTransform saveTransform = g2d.getTransform();
+        Path2D room = new Path2D.Double();
+        boolean isFirst = true;
         for(Pair<Double,Double> pair:coordinates){
-            if(lastX == null || lastY == null) {
-                lastX = pair.getKey();
-                lastY = pair.getValue();
+            Double x = pair.getKey();
+            Double y = pair.getValue();
+            if(isFirst) {
+                room.moveTo(x,y);
+                isFirst = false;
             }else{
-                Double x = pair.getKey();
-                Double y = pair.getValue();
-                g2d.draw(new Line2D.Double(lastX-minX,lastY-minY,x-minX,y-minY));
-                lastX = x;
-                lastY = y;
+                room.lineTo(x,y);
             }
+
         }
-
-
-
+        room.closePath();
+        //Scales the room to our needs
+        try {
+            AffineTransform scaleMatrix = new AffineTransform();
+            scaleMatrix.translate(STARTX,STARTY);
+            scaleMatrix.scale(SCALE, SCALE);
+            g2d.setTransform(scaleMatrix);
+            g2d.draw(room);
+        } finally {
+            g2d.setTransform(saveTransform);
+        }
     }
 
     public ArrayList<Pair<Double, Double>> getCoordinates() {
--- visualiser/src/test/java/RoomTest.java
+++ visualiser/src/test/java/RoomTest.java
@@ -10,7 +10,7 @@ public class RoomTest {
 
     @Test
     public void areCoordinatesSaved(){
-        String coordinates = "(0,0), (2,0), (0,0), (2,1), (1,1), (1,2), (0,2)";
+        String coordinates = "(0, 0), (2, 0), (0, 0), (2, 1), (1, 1), (1, 2), (0, 2)";
         Room room = new Room(coordinates);
         ArrayList<Pair<Double, Double>> pairs = new ArrayList<>();
         pairs.add(new Pair<>(0.0,0.0));
