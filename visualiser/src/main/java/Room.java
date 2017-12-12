5f145dc986ac7ccc4f0554235df9a5c6f8648e18
Anthony Cheng <anthonyhlcheng@gmail.com>
2017-12-12 13:26:03 +0000

    Finished adding Furniture to the room. There are also helper classes to assist in making other furniture in the future

--- visualiser/src/main/java/Room.java
+++ visualiser/src/main/java/Room.java
@@ -14,54 +14,41 @@ import java.util.Scanner;
 class Room extends JPanel {
 
     private ArrayList<Pair<Double,Double>> coordinates = new ArrayList<>();
-    private Double minX = -50.0;
-    private Double minY = -50.0;
-    private static final Double SCALE = 10.0;
-    private static final Double STARTX = 50.0;
-    private static final Double STARTY = 50.0;
+    private ArrayList<String> shapesInRoom = new ArrayList<>();
+    private static final Double SCALE = 1.0; //PAPER SCALE
+    private final Double STARTX = 20.0;
+    private final Double STARTY = 20.0;
 
-    Room(String room){
+    Room(String room, ArrayList<String> shapesInRoom){
         //The string room is formatted as follows: (0,0), (0,1), (0,2) and so on...
-        room = room.replace(" ","").replace("),(", ")/(");
-        String[] coordinateArray = room.split("/");
-        for(String coordinate:coordinateArray){
-            System.out.println(coordinate);
-            int commaLocation = coordinate.indexOf(",");
-            Double x = Double.parseDouble(coordinate.substring(1,commaLocation));
-            Double y = Double.parseDouble(coordinate.substring(commaLocation+1, coordinate.length()-1));
-            if(x < minX){
-                minX = x;
-            }
-            if(y < minY){
-                minY = y;
-            }
-            coordinates.add(new Pair<>(x,y));
-        }
+        StringToCoordinates converter = new StringToCoordinates(room);
+        coordinates = converter.getCoordinates();
+        this.shapesInRoom = shapesInRoom;
     }
     private void drawRoom(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         AffineTransform saveTransform = g2d.getTransform();
-        Path2D room = new Path2D.Double();
-        boolean isFirst = true;
-        for(Pair<Double,Double> pair:coordinates){
-            Double x = pair.getKey();
-            Double y = pair.getValue();
-            if(isFirst) {
-                room.moveTo(x,y);
-                isFirst = false;
-            }else{
-                room.lineTo(x,y);
-            }
+        ShapeGenerator generator = new ShapeGenerator();
+        generator.create(coordinates);
+        Path2D room = generator.getPaths().get(0);
 
-        }
-        room.closePath();
+        //Place furniture in room
+        ShapeGenerator furnitureGenerator = new ShapeGenerator(shapesInRoom);
+        ArrayList<Path2D> furnitures = furnitureGenerator.getPaths();
         //Scales the room to our needs
         try {
             AffineTransform scaleMatrix = new AffineTransform();
-            scaleMatrix.translate(STARTX,STARTY);
+            scaleMatrix.translate(STARTX-generator.minX,STARTY-generator.minY);
             scaleMatrix.scale(SCALE, SCALE);
             g2d.setTransform(scaleMatrix);
             g2d.draw(room);
+
+            //Colouring in the furniture inside the room - this is only an example
+            for(Path2D furniture:furnitures){
+                g2d.setPaint(Color.BLACK);
+                g2d.fill(furniture);
+                g2d.draw(furniture);
+            }
         } finally {
             g2d.setTransform(saveTransform);
         }
