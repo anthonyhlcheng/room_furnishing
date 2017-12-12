5d5b292acfee34021d15a5d6bdf96039b931d441
Gavin Shek <gavgav-s@hotmail.com>
2017-12-12 23:54:53 +0000

    Algorithm improvements

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # room_furnishing
 The Room Furnishing Problem 
 
+## Versions
+- Version 0 - Initial algorithm
+- Version 1 - Initial algorithm with efficiency improvements
 
 ### Requirements for algorithm
 - Shapely (https://shapely.readthedocs.io/en/latest/project.html#requirements)
 - Matplotlib (https://matplotlib.org/users/installing.html)
 
 ### To run algorithm on Linux
-`./algorithm/main.py problems.rfp output.txt`
+`./algorithm/main.py problems.rfp outputs/output_{}.txt` [Version Number]
 
-*Output will then be stored in the file __output.txt__*
+*Output will then be stored in the file __ouputs/output_[Version Number].txt__*
--- algorithm/main.py
+++ algorithm/main.py
@@ -5,22 +5,25 @@ import algorithm
 import time
 import sys
 
-def start(input_file, output_file):
+def start(input_file, output_file, version):
+    output_file
     with open(output_file, "w") as output:
         output.write("zaragoza\n")
         output.write("t2ri0va94ush0tdu9gpuusq64r\n")
-        counter = 1
-        for problem in parser.main(input_file):
-            start_time = time.time()
-            print("Problem {}".format(counter), end="")
-            sys.stdout.flush()
-            output.write("{}: {}\n".format(counter, algorithm.solve_problem(problem)))
-            print(" - {} seconds".format(round(time.time() - start_time, 2)))
-            counter += 1
+    counter = 1
+    for problem in parser.main(input_file):
+        start_time = time.time()
+        print("Problem {}".format(counter), end="\r")
+        sys.stdout.flush()
+        with open(output_file, "a") as output:
+            output.write("{}: {}\n".format(counter, algorithm.solve_problem(counter, version, problem)))
+        print("Problem {} - {} seconds".format(counter, round(time.time() - start_time, 2)))
+        counter += 1
 
 if __name__ == "__main__":
+    start(sys.argv[1], sys.argv[2].format(sys.argv[3]), sys.argv[3])
     try:
-        start(sys.argv[1], sys.argv[2])
+        start(sys.argv[1], sys.argv[2].format(sys.argv[3]), sys.argv[3])
     except Exception as e:
         print(e)
-        print("Usage: {} [input filename] [output filename]".format(sys.argv[0]))
+        print("Usage: {} [input filename] [output filename with {} inside] [version number]".format(sys.argv[0]))
