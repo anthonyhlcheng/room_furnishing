8ed7168b6bada4a5d48c699d8693ade85ff40fbd
Gavin Shek <gavgav-s@hotmail.com>
2017-12-13 02:09:33 +0000

    Added coverage calculator

---
+++ algorithm/coverage_calculator.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+from shapely.geometry import Polygon
+import parser
+import sys
+
+# Parameters:
+#   input_file: string
+#   output_file: string
+#   number: integer
+# Returns:
+#   (coverage, resulting_cost)
+#       where coverage: double
+#             resulting_cost: double
+def go(input_file, output_file, number):
+    problem_info = {}
+    for i in [input_file, output_file]:
+        with open(i, "r") as in_file:
+            for problem in in_file:
+                if problem.split(":")[0].isdigit():
+                    if int(problem.split(":")[0]) == number:
+                        problem_info[i] = problem
+                        break
+    return calculate(parser.divide_problem(problem_info[input_file]), parser.parse_result(problem_info[output_file]))
+
+# Parameters:
+#   original_problem: (room, furniture)
+#       where room: [coordinates]
+#             furniture: [(cost, [coordinates])]
+#             cost: integer
+#             coordinates: (double, double)
+#   result: [[coordinates]]
+# Returns:
+#   (coverage, resulting_cost)
+#       where coverage: double
+#             resulting_cost: double
+def calculate(original_problem, result):
+    coverage = 0
+    resulting_cost = 0
+    costs = {round(Polygon(f).area, 5) : cost for cost,f in original_problem[1]}
+    for i in map(Polygon, result):
+        coverage += i.area
+        resulting_cost += costs[round(i.area, 5)] * i.area
+    coverage /= Polygon(original_problem[0]).area
+    coverage *= 100
+    return (round(coverage, 2), round(resulting_cost, 3))
+
+if __name__ == "__main__":
+    try:
+        print(go(sys.argv[1], sys.argv[2], int(sys.argv[3])))
+    except Exception as e:
+        print(e)
+        print("Usage: {} [problem filename] [output filename] [problem number]")
--- algorithm/parser.py
+++ algorithm/parser.py
@@ -3,15 +3,22 @@
 import sys
 
 # Returns:
-# (room, furniture)
-#   room: [(double,double)] // list of coordinates
-#   furniture: [(int, [(double, double)]]
-#       list of tuples, tuples contain cost and list of coordinates
+#   (room, furniture)
+#       where room: [(double,double)] // list of coordinates
+#             furniture: [(int, [(double, double)]]
+#               // list of tuples, tuples contain cost and list of coordinates
 def divide_problem(problem):
     room = "".join(problem.split("#")[0].split(":")[1:]).strip()
     room = [tuple(map(float, i.replace(" ", "").strip("()").split(","))) for i in room.split("),")]
     furniture = [(int(i.strip().split(":")[0]), [tuple(map(float, j.replace(" ", "").strip("()").split(","))) for j in i.strip().split(":")[1].split("),")]) for i in problem.split("#")[1].split(";")]
     return (room, furniture)
+
+# Returns:
+#   [[coordinates]]
+#       where coordinates: (double, double)
+def parse_result(problem):
+    return [[tuple(map(float, j.replace(" ", "").strip("()").split(","))) for j in i.split("),")] for i in problem.split(":")[1].strip().split("; ")]
+
 
 # Returns list:
 # [(room, temperature)]
